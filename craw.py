181b4b29d7ff6b9a460b4595cd1d7c96f37d11d4
JaKyung <57945595+newmade01@users.noreply.github.com>
2020-12-07 20:42:16 +0900

    Update craw.py

--- craw.py
+++ craw.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python3
-# Anchor extraction from HTML document
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-with urlopen('https://en.wikipedia.org/wiki/Main_Page') as response:
-    soup = BeautifulSoup(response, 'html.parser')
-    for anchor in soup.find_all('a'): #a태그
-        print(anchor.get('href', '/')) #조소를 반복적으로 가져옴
-
-        
-        
+response=urlopen('https://trends.google.co.kr/trends/trendingsearches/daily?geo=KR') 
+soup = BeautifulSoup(response, 'html.parser')
+i=1
+f=open("새파일.text", 'w')
+for anchor in soup.find_all('span',class_='title title-break'):
+    data=str(i)+"위: "+anchor.get_text()+"\n" #조소를 반복적으로 가져옴    
+    i = i + 1
+    f.write(data)
+f.close()
+      
